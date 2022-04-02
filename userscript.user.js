48ee48f2161eb7214157c2577fb3d051ca99b930
ActuallyShip <ActuallyShip@gmail.com>
2022-04-02 21:13:11 +0200

    Credits To Alg

--- userscript.user.js
+++ userscript.user.js
@@ -16,6 +16,15 @@ if (window.top !== window.self) {
             i.src = "https://algoinde.ru/f/1231231.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
+            function a() {
+              i.style.display = 'none';
+              setTimeout(b, 1000);
+            }
+            function b() {
+              i.style.display = null;
+              setTimeout(a, 1000);
+            }
+            b();
             return i;
         })())
 
