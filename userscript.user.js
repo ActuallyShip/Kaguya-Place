2d4260cc42136266a584cef0e5a3502873213a3b
ActuallyShip <ActuallyShip@gmail.com>
2022-04-03 21:07:50 +0200

    update

--- userscript.user.js
+++ userscript.user.js
@@ -14,7 +14,7 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("img");
             i.src = "https://algoinde.ru/f/1231231.png?";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px; transition: opacity 0.2s ease";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px; transition: opacity 0.2s ease";
             console.log(i);
             var t;
             function a() {
